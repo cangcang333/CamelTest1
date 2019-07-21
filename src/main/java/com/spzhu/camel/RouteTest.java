c1a80310ca1215029c35ad800981f0fd876b2c07
zhushaopeng <cangcang333@163.com>
2019-07-22 00:56:46 +0800

    add slf4j logger

--- src/main/java/com/spzhu/camel/RouteTest.java
+++ src/main/java/com/spzhu/camel/RouteTest.java
@@ -3,11 +3,17 @@ package com.spzhu.camel;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.impl.DefaultCamelContext;
 import org.apache.camel.model.ModelCamelContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class RouteTest extends RouteBuilder {
 
 
+
     public static void main(String[] args) throws Exception {
+        Logger Log = LoggerFactory.getLogger(RouteTest.class);
+        Log.debug("In main()");
+
         ModelCamelContext modelCamelContext = new DefaultCamelContext();
         modelCamelContext.start();
         modelCamelContext.addRoutes(new RouteTest());
@@ -19,6 +25,8 @@ public class RouteTest extends RouteBuilder {
 
     @Override
     public void configure() {
+        Logger Log = LoggerFactory.getLogger(RouteTest.class);
+        Log.debug("In configure()");
         /*
         restConfiguration()
                 .host("localhost")
