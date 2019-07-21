5519291f85be4873cc9b8a6473d5a4eddf419d11
zhushaopeng <cangcang333@163.com>
2019-07-22 00:12:08 +0800

    add direct route A

---
+++ src/main/java/com/spzhu/camel/DirectRouteA.java
@@ -0,0 +1,9 @@
+package com.spzhu.camel;
+
+import org.apache.camel.builder.RouteBuilder;
+
+public class DirectRouteA extends RouteBuilder {
+    public void configure() throws Exception {
+
+    }
+}
